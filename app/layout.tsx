bfec1096a015ed147e2364fe2ff9cb5f619f0a04
aminbenz <aminbenz.dev@gmail.com>
2023-11-04 12:48:53 +0100

    add postcssfile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
